d4660bebb7c0ef3cce706422e5ccfb8f17f21be2
Malash Dzmitrij <d.malash@flex.by>
2018-02-21 11:41:28 +0300

    C:/Program Files/Git/no-barba

--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,7 @@ var BarbaWitget = {
     window.DOM.getScrollWidth();
     preventDbClick();
     Barba.Pjax.start();
-    Barba.Prefetch.init();
+    // Barba.Prefetch.init();
 
     /* 
       ** проверяем урл (внешний переход или битрикс. или внутренний)
@@ -50,7 +50,7 @@ var BarbaWitget = {
     Barba.Dispatcher.on('linkClicked', function(elem) {
       let _t = $(elem);
       if(_t.attr('href').indexOf('/bitrix/admin/') !== -1 || _t.hasClass('no-barba')) {
-        window.location.href = window.location.host+$(this).attr('href');
+        window.location.href = window.location.host+_t.attr('href');
       } else {
         if(_t.hasClass('business-request')) {
           business = true;
